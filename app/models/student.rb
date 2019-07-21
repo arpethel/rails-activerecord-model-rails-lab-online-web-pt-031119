492c167b6a1d2d2a370f00f42b74c01923d27e0c
Ryan Pethel <arpethel@gmail.com>
2019-07-21 20:22:42 +0000

    Automatically backed up by Learn

--- app/models/student.rb
+++ app/models/student.rb
@@ -2,5 +2,6 @@ class Student < ActiveRecord::Base
   # binding.pry
   # @student.to_s = Student.first_name + " " + Student.last_name
   @student = Student.create!(first_name: "Daenerys", last_name: "Targaryen")
-  @student.to_s = @student.first_name + " " + @student.last_name
+  @student = @student.first_name + " " + @student.last_name
+  @student.to_s
 end
