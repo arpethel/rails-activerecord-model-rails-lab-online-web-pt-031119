ff86953d3cc6e8ed5b3cdd00ee91136ab4c0d4b6
Ryan Pethel <arpethel@gmail.com>
2019-07-21 20:59:34 +0000

    Automatically backed up by Learn

--- spec/models/student_spec.rb
+++ spec/models/student_spec.rb
@@ -3,7 +3,6 @@ require 'rails_helper'
 describe Student do
   before(:each) do
     @student = Student.create!(first_name: "Daenerys", last_name: "Targaryen")
-    @student = Student.create!(first_name: "Flo", last_name: "Smith")
   end
 
   it 'can be created' do
@@ -12,9 +11,5 @@ describe Student do
 
   it 'has a to_s instance method' do
     expect(@student.to_s).to eq("Daenerys Targaryen")
-  end
-
-  it 'has a to_s instance method' do
-    expect(@student.to_s).to eq("Flo Smith")
   end
 end
